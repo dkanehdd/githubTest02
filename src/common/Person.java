15f14690c5379ad5444e7d18706e791f73cad367
dkanehdd <dkanehdd@naver.com>
2020-11-06 18:07:48 +0900

    충돌해결

--- src/common/Person.java
+++ src/common/Person.java
@@ -3,8 +3,17 @@ package common;
 public class Person {
 	String name;
 	int age;
-	String nickName;
-	
+//	String nickName;
+//	String nickName1;
+//	String nickName;2
+//	String nickName;3
+//	String nickName;4
+//	String nickName;45
+//	String nickName;56
+//	String nickName;6
+//	String nickName;78
+//	String nickName;ㅑ
+//	
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
